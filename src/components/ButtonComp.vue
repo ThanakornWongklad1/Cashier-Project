5f4495fdd9c07c03f1b17a2725a89b82898d8cdb
Thanakorn Wongklad <thanakorn.wong@kmutt.ac.th>
2023-03-28 01:48:29 +0700

    first commit

---
+++ src/components/ButtonComp.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+const props = defineProps({
+    buttName: {
+        type: String,
+        require: true
+    },
+    buttFunc: {
+        type: Function,
+        require: true
+    },
+    parameter:{
+        require:false
+    }
+})
+
+</script>
+ 
+<template>
+    <div>
+        <button class="w-24 text-center py-3 "  @click="buttFunc(parameter)">{{ buttName }}</button>
+    </div>
+</template>
---
+++ src/components/ListComp.vue
@@ -0,0 +1,28 @@
+<script setup>
+import trash from '../icon/OcticonTrash16.vue'
+
+
+const emits = defineEmits(['deleteList'])
+const props = defineProps({ 
+    addLists: {
+        type: Array,
+        require: true,
+    },
+
+})
+
+
+
+</script>
+ 
+<template>
+    <div v-for=" (list, index) in addLists">
+        <div class=" flex flex-row my-2">
+            <span class="mr-3">{{ index + 1 }}.</span>
+            <span>{{ list }} ฿</span>
+            <trash class="ml-auto text-red-600" @click="$emit('deleteList', index)" />
+        </div>
+        <hr>
+    </div>
+</template>
+ 
